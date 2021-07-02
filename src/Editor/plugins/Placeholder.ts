804a328b24e309c3eaa1aa68a785952a9beab4d6
LiYechao <liyechao@yechao.xyz>
2021-07-02 23:29:01 +0800

    fix: Placeholder only add to paragraph node

--- src/Editor/plugins/Placeholder.ts
+++ src/Editor/plugins/Placeholder.ts
@@ -1,4 +1,5 @@
 import { Plugin } from 'prosemirror-state'
+import { findChildren } from 'prosemirror-utils'
 import { Decoration, DecorationSet } from 'prosemirror-view'
 import Extension from '../lib/Extension'
 
@@ -13,33 +14,32 @@ export default class Placeholder extends Extension {
         props: {
           decorations: state => {
             const decorations: Decoration[] = []
-            let isBodyEmpty = true
-            let bodyPlaceholderPos = -1
 
-            state.doc.forEach((node, offset) => {
-              if (node.type.name === 'title') {
-                if (node.textContent.trim().length === 0) {
-                  const placeholder = document.createElement('span')
-                  placeholder.setAttribute('data-placeholder', 'Untitled')
-                  placeholder.classList.add('ProseMirror-placeholder')
-                  decorations.push(Decoration.widget(offset + 1, placeholder))
-                }
-              } else {
-                if (bodyPlaceholderPos === -1) {
-                  bodyPlaceholderPos = offset + 1
-                }
-
-                if (!node.isTextblock || node.textContent.trim().length > 0) {
-                  isBodyEmpty = false
-                }
-              }
-            })
-
-            if (isBodyEmpty && bodyPlaceholderPos >= 0) {
+            const title = state.doc.firstChild
+            if (title?.type.name === 'title' && title.textContent.trim().length === 0) {
               const placeholder = document.createElement('span')
-              placeholder.setAttribute('data-placeholder', 'Write something...')
+              placeholder.setAttribute('data-placeholder', 'Untitled')
               placeholder.classList.add('ProseMirror-placeholder')
-              decorations.push(Decoration.widget(bodyPlaceholderPos, placeholder))
+              decorations.push(Decoration.widget(1, placeholder))
+            }
+
+            const hasContent =
+              findChildren(
+                state.doc,
+                n => n.type.name !== 'title' && n.isTextblock && n.textContent.trim().length > 0
+              ).length > 0
+
+            if (!hasContent) {
+              const pos = findChildren(
+                state.doc,
+                n => n.type.name === 'paragraph' && n.textContent.trim().length === 0
+              )[0]?.pos
+              if (pos !== undefined) {
+                const placeholder = document.createElement('span')
+                placeholder.setAttribute('data-placeholder', 'Write something...')
+                placeholder.classList.add('ProseMirror-placeholder')
+                decorations.push(Decoration.widget(pos, placeholder))
+              }
             }
 
             return DecorationSet.create(state.doc, decorations)
