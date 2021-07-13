d7ab3d537149bf80f9309197ec35342ef70b0e2c
LiYechao <liyechao@yechao.xyz>
2021-07-13 22:15:32 +0800

    Add license

--- scripts/dist/config/webpack.config.app.build.js
+++ scripts/dist/config/webpack.config.app.build.js
@@ -1,4 +1,17 @@
 "use strict";
+// Copyright 2021 LiYechao
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -64,6 +77,12 @@ const configuration = {
         }),
         new html_webpack_plugin_1.default({ template: 'index.html' }),
         new clean_webpack_plugin_1.CleanWebpackPlugin(),
+        new monaco_editor_webpack_plugin_1.default({
+            filename: 'js/[name].worker.js',
+        }),
+        new copy_webpack_plugin_1.default({
+            patterns: [{ from: 'static', to: 'static' }],
+        }),
         new terser_webpack_plugin_1.default({
             parallel: true,
             terserOptions: {
@@ -72,12 +91,6 @@ const configuration = {
                 },
             },
             extractComments: false,
-        }),
-        new monaco_editor_webpack_plugin_1.default({
-            filename: 'js/[name].worker.js',
-        }),
-        new copy_webpack_plugin_1.default({
-            patterns: [{ from: 'static', to: 'static' }],
         }),
     ],
 };
